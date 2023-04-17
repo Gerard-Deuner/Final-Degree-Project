19533a7c24f6dba424d04f154e271678e9d8ee02
Gerard-Deuner <104646267+Gerard-Deuner@users.noreply.github.com>
2023-04-17 16:44:38 +0200

    Update Pando.R

--- Pando.R
+++ Pando.R
@@ -2,11 +2,14 @@
 # PANDO #
 #########
 
+# https://quadbiolab.github.io/Pando/
+
 # Load Packages
 library(Pando)
 library(Seurat)
 library(BSgenome.Hsapiens.UCSC.hg38)
 library(qs)
+library(Signac)
 
 # Define dataset  
 dataset <- "timecourse" # timecourse | combined
@@ -15,16 +18,28 @@ dataset <- "timecourse" # timecourse | combined
 data.dir <- paste0("/g/scb/zaugg/deuner/GRaNIE/tmp/", dataset, ".pp.seuratObject.qs")
 
 # Load seurat object
-seurat_object <- qread
+seurat_object <- qread(data.dir)
 
 # Get motif data
 data(motifs)
+
+# Add genome annotation for ChromatinAssay
+DefaultAssay(seurat_object) <- "ATAC"
+genome(seurat_object) <- "hg38"
+
+# Add annotation
+## get peak ranges (timecourse)
+peakRanges <- qread("/g/scb/zaugg/marttine/dariaMultiome/peakRanges.timecourse.qs")
+Annotation(seurat_object[["ATAC"]]) <- peakRanges
 
 # Select variable features
 seurat_object <- Seurat::FindVariableFeatures(seurat_object, assay='RNA')
 
 # Initiate GRN object and select candidate regions
-seurat_object <- initiate_grn(seurat_object)
+seurat_object <- initiate_grn(seurat_object, 
+                              peak_assay = "ATAC",
+                              rna_assay = "RNA",
+                              exclude_exons = T)
 
 # Scan candidate regions for TF binding motifs
 seurat_object <- find_motifs(
@@ -43,4 +58,7 @@ coef(seurat_object)
 test_srt <- find_modules(test_srt)
 
 # Print modules
-NetworkModules(test_srt)
+modules <- NetworkModules(test_srt)
+
+# Save Network output
+write.csv(modules@meta, paste0("/g/scb/zaugg/deuner/Pando/outputdata/", dataset, ".GRN.tsv"))
