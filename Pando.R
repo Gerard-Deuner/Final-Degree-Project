2c42482c862134913afc6b3334c14a1e8f0cd748
Gerard-Deuner <104646267+Gerard-Deuner@users.noreply.github.com>
2023-04-18 15:53:25 +0200

    Update Pando.R

--- Pando.R
+++ Pando.R
@@ -48,8 +48,30 @@ seurat_object <- find_motifs(
   genome = BSgenome.Hsapiens.UCSC.hg38
 )
 
+# Code that fixes an error ecountered in infer_grn(). It inseured a one-to-one mapping between cand_regions and peak_ranges.
+regions <- NetworkRegions(seurat_object)
+cand_ranges <- peakRanges@ranges
+peak_ranges <- StringToGRanges(rownames(GetAssay(seurat_object, assay='ATAC')))
+peak_overlaps <- findOverlaps(cand_ranges, peak_ranges)
+
+peak_overlaps_df <- data.frame(peak_overlaps)
+
+peak_overlaps_df <- peak_overlaps_df[!duplicated(peak_overlaps_df[['queryHits']]), ]
+peak_overlaps_df <- peak_overlaps_df[!duplicated(peak_overlaps_df[['subjectHits']]), ]
+
+new_motif_data <- peakRanges@motifs@data[peak_overlaps_df[['queryHits']], ]
+new_regions_peaks <- peak_overlaps_df[['subjectHits']]
+new_ranges_data <- peakRanges@ranges[peak_overlaps_df[['queryHits']]]
+
+seurat_object@grn@regions@motifs@data <- new_motif_data
+seurat_object@grn@regions@peaks <- new_regions_peaks
+seurat_object@grn@regions@ranges <- new_ranges_data
+
 # Infer gene regulatory network
-seurat_object <- infer_grn(seurat_object)
+seurat_object <- infer_grn(seurat_object,
+                           peak_to_gene_method = 'Signac',
+                           method = 'glm',
+                           parallel = TRUE)
 
 # Print inferred coefficients
 coef(seurat_object)
@@ -62,3 +84,6 @@ modules <- NetworkModules(test_srt)
 
 # Save Network output
 write.csv(modules@meta, paste0("/g/scb/zaugg/deuner/Pando/outputdata/", dataset, ".GRN.tsv"))
+
+overlaps <- findOverlaps(seurat_object@assays$ATAC@ranges, seurat_object@assays$ATAC@ranges)
+all(data.frame(overlaps)[['queryHits']] == data.frame(overlaps)[['subjectHits']])
