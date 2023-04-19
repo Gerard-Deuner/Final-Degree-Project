758b80892a2a97c1893fa385f69821a05d4d3546
Gerard-Deuner <104646267+Gerard-Deuner@users.noreply.github.com>
2023-04-19 10:50:04 +0200

    Update MergePeakGeneQCPDFs.sh

--- MergePeakGeneQCPDFs.sh
+++ MergePeakGeneQCPDFs.sh
@@ -13,7 +13,7 @@ mkdir $path${1}_batch_mode_${2}/peak_Gene_diagnosticPlots_all
 res=("0.1" "0.5" "0.25" "0.75" "1" "2" "3" "4" "5" "6" "7" "8" "9" "10" "12" "14" "16" "18" "20")
 
 # se titerable variable
-i=1
+i=0
 
 # iterate over folders
 for dir in $path${1}"_batch_mode_"${2}"/Batch_Mode_Outputs/"
@@ -33,4 +33,4 @@ done
 
 # merge all PDFs
 cd $path${1}_batch_mode_${2}/peak_Gene_diagnosticPlots_all/
-gs -dFirstpage=1 -dLastPage=1 -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=/g/scb/zaugg/deuner/GRaNIE/outputdata/batch_mode/$1_batch_mode_$2/peak_Gene_diagnosticPlots_all/peak_Gene_diagnosticPlots_all_MERGED.pdf *.pdf
+gs -dFirstpage=1 -dLastPage=1 -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=/g/scb/zaugg/deuner/GRaNIE/outputdata/batch_mode/$1_batch_mode_$2/peak_Gene_diagnosticPlots_all/peak_Gene_diagnosticPlots_all_MERGED.pdf $(ls -v)
