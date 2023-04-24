0be61be389a73a33a7c63bbad738d77aa59f5cad
Gerard-Deuner <104646267+Gerard-Deuner@users.noreply.github.com>
2023-04-24 11:45:28 +0200

    Create SLURM_ChiPSeq_setup.sh

---
+++ SLURM/SLURM_ChiPSeq_setup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH --job-name CHIPSEQ_SETUP
+#SBATCH -A zaugg
+#SBATCH --nodes 1
+#SBATCH --ntasks 16
+#SBATCH --mem 128GB
+#SBATCH --time 24:00:00
+#SBATCH --output output_chipseq_setup.txt
+#SBATCH --error error_chipseq_setup.txt
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=gerard.deuner@embl.de 
+
+echo "I, ${USER}, am running on host ${HOSTNAME}"
+ 
+echo "STARTING JOB"
+ 
+# Load R module
+module load R
+module load R-bundle-Bioconductor/3.15-foss-2021b-R-4.2.0
+ 
+ 
+# Executing an R script
+Rscript --vanilla /g/scb/zaugg/deuner/valdata/code/ChiPSeq_setup.R timecourse
+Rscript --vanilla /g/scb/zaugg/deuner/valdata/code/ChiPSeq_setup.R combined
+
+sleep 600
+
+echo "FINISHED JOB"
