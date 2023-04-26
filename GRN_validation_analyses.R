380b479650c99422d90cbb49267f01009f8f7c54
Gerard-Deuner <104646267+Gerard-Deuner@users.noreply.github.com>
2023-04-26 15:21:09 +0200

    Update GRN_validation_analyses.R

--- GRN_validation_analyses.R
+++ GRN_validation_analyses.R
@@ -4,6 +4,10 @@
 
 ## Experimental data used: pcHI-C, ChiP-Seq and eQTL
 
+#%%%%%%%#
+# SETUP #
+#%%%%%%%#
+
 ## input data format (one df per exp data)
 ## rows: links in GRN (ids) , columns: gene/TF, peak, resolution, validated, setting, dataset, corr.method,  
 #         gene/TF         peak          resolution      validated            setting           dataset         corr.method     
@@ -11,6 +15,9 @@
 
 # load libraries
 library(dplyr)
+library(tidyr)
+library(purrr)
+library(stringr)
 
 # define datasets
 datasets <- c("timecourse", "combined")
@@ -18,19 +25,22 @@ corr.methods <- c("pearson", "spearman")
 val.data <- c("pcHi-C", "ChiP-seq", "eQTL")
 
 # set validation data paths
-pcHiC.dir <- "/g/scb/zaugg/deuner/valdata/pcHi-C/results/"
-ChiP.dir <- "/g/scb/zaugg/deuner/valdata/ChiP-seq/results/"
-eQTL.dir <- "/g/scb/zaugg/deuner/valdata/eQTL/results/"
+pcHiC.dir <- "/g/scb/zaugg/deuner/valdata/pcHi-C/validated_links/"
+ChiP.dir <- "/g/scb/zaugg/deuner/valdata/ChiP-seq/validated_links/"
+eQTL.dir <- "/g/scb/zaugg/deuner/valdata/eQTL/validated_links/"
 
 # get pcHi-C data
-pcHiC.df <- as.data.frame(matrix(nrow = 0, ncol = 5))
+pcHiC.df <- as.data.frame(matrix(nrow = 0, ncol = 7))
 names(pcHiC.df) <- c("gene", "peak", "res", "validated", "setting", "dataset", "corr.method")
 
 for (file in list.files(pcHiC.dir)){
-  df <- read.csv(paste0(pcHiC.dir, file))
+  df <- read.csv(paste0(pcHiC.dir, file), row.names = 1)
+  dataset <- str_split(file, "_") %>% map(1)
+  corr.method <- str_split(file, "_") %>% map(2)
   df <- df %>%
-    mutate(setting = rep(file, nrow(df)))
-  df[c("dataset", "corr.method")] <- str_split_fixed(df$setting, "_", 2)
+    tibble::add_column(setting = paste(dataset, corr.method, sep = "_"), 
+                      dataset = dataset,
+                      corr.method = corr.method)
   pcHiC.df <- rbind(pcHiC.df, df)
 }
 
@@ -40,9 +50,12 @@ names(ChiP.df) <- c("TF", "peak", "res", "validated", "setting", "dataset", "cor
 
 for (file in list.files(ChiP.dir)){
   df <- read.csv(paste0(ChiP.dir, file))
+  dataset <- str_split(file, "_") %>% map(1)
+  corr.method <- str_split(file, "_") %>% map(2)
   df <- df %>%
-    mutate(setting = rep(file, nrow(df)))
-  df[c("dataset", "corr.method")] <- str_split_fixed(df$setting, "_", 2)
+    tibble::add_column(setting = paste(dataset, corr.method, sep = "_"), 
+                       dataset = dataset,
+                       corr.method = corr.method)
   ChiP.df <- rbind(ChiP.df, df)
 }
 
@@ -52,9 +65,12 @@ names(eQTL.df) <- c("gene", "peak", "res", "validated", "setting", "dataset", "c
 
 for (file in list.files(eQTL.dir)){
   df <- read.csv(paste0(eQTL.dir, file))
+  dataset <- str_split(file, "_") %>% map(1)
+  corr.method <- str_split(file, "_") %>% map(2)
   df <- df %>%
-    mutate(setting = rep(file, nrow(df)))
-  df[c("dataset", "corr.method")] <- str_split_fixed(df$setting, "_", 2)
+    tibble::add_column(setting = paste(dataset, corr.method, sep = "_"), 
+                       dataset = dataset,
+                       corr.method = corr.method)
   eQTL.df <- rbind(eQTL.df, df)
 }
 
@@ -65,5 +81,3 @@ val.df$validation <- rep(c("pcHi-C", "ChiP-seq", "eQTL"), times = c(nrow(pcHiC.d
 #%%%%%%%#
 # PLOTS #
 #%%%%%%%#
-
-# example data
