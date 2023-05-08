5a683dcf49cd0c609a77a4630f59fbd8432b7510
Gerard-Deuner <104646267+Gerard-Deuner@users.noreply.github.com>
2023-05-08 10:04:32 +0200

    Create get_fragments_from_archr.R

---
+++ get_fragments_from_archr.R
@@ -0,0 +1,35 @@
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+# Get ATAC fragments from ArchR Project #
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%#
+
+# Install ArchR
+# devtools::install_github("GreenleafLab/ArchR", ref="master", repos = BiocManager::repositories())
+
+# Load libraries
+library("ArchR")
+library("qs")
+
+# Set ArchR Project directory
+archr_dir <- "/g/scb/zaugg/marttine/dariaMultiome/ArchR/timecourse/ArchRproject.qs"
+
+# Load ArchR Project
+ArchRProj <- qread(archr_dir)
+# ArchRProj = loadArchRProject(path = archr_dir, force = FALSE, showLogo = TRUE)
+
+# Get the fragments
+fragments <- getFragmentsFromProject(
+  ArchRProj = ArchRProj,
+)
+
+# Have a look at them
+fragments[[1]]
+
+# Save them into a tsv file
+write.table(x = fragments[[1]], file = "/g/scb/zaugg/deuner/SCENIC+/inputdata/timecourse_fragments.tsv", col.names=FALSE, row.names=FALSE, quote=FALSE, sep = "\t")
+
+# Then convert the strand column from "*" to "+" by; sed 's/\*/+/g' timecourse_fragments.tsv > timecourse_fragments_translated.csv 
+# Reorder the columns by: awk '{ print $1"\t"$2"\t"$3"\t"$6"\t"$4"\t"$5 }' timecourse_fragments_translated.csv > timecourse_fragments_translated_ordered.tsv
+# Change "#" for "_": sed 's/\#/_/g' timecourse_fragments_translated_ordered.tsv > timecourse_fragments_translated_ordered2.tsv
+# Sort them: sort -t$'\t' -k1 -k2 timecourse_fragments_translated_ordered3.tsv > timecourse_fragments_translated_ordered4.tsv
+# BED format: http://www.ensembl.org/info/website/upload/bed.html
+# Add "timecourse_": 
